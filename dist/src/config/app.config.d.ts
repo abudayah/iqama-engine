7e3d597e5ca1232338ffc66bbc58799be9858f77
Anas Abu dayah <anas.abudayah@gmail.com>
2026-05-02 12:57:25 -0700

    migrate to ts 6.0

--- dist/src/config/app.config.d.ts
+++ dist/src/config/app.config.d.ts
@@ -1,15 +1,12 @@
 export declare const appConfig: (() => {
+  masjidLatitude: number;
+  masjidLongitude: number;
+  masjidTimezone: string;
+  databaseUrl: string | undefined;
+}) &
+  import('@nestjs/config').ConfigFactoryKeyHost<{
     masjidLatitude: number;
     masjidLongitude: number;
     masjidTimezone: string;
-    upstashRedisUrl: string | undefined;
-    upstashRedisToken: string | undefined;
     databaseUrl: string | undefined;
-}) & import("@nestjs/config").ConfigFactoryKeyHost<{
-    masjidLatitude: number;
-    masjidLongitude: number;
-    masjidTimezone: string;
-    upstashRedisUrl: string | undefined;
-    upstashRedisToken: string | undefined;
-    databaseUrl: string | undefined;
-}>;
+  }>;
--- dist/src/config/app.config.js
+++ dist/src/config/app.config.js
@@ -1,13 +1,11 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.appConfig = void 0;
-const config_1 = require("@nestjs/config");
+const config_1 = require('@nestjs/config');
 exports.appConfig = (0, config_1.registerAs)('app', () => ({
-    masjidLatitude: parseFloat(process.env.MASJID_LATITUDE ?? '49.2514'),
-    masjidLongitude: parseFloat(process.env.MASJID_LONGITUDE ?? '-122.7740'),
-    masjidTimezone: process.env.MASJID_TIMEZONE ?? 'America/Vancouver',
-    upstashRedisUrl: process.env.UPSTASH_REDIS_URL,
-    upstashRedisToken: process.env.UPSTASH_REDIS_TOKEN,
-    databaseUrl: process.env.DATABASE_URL,
+  masjidLatitude: parseFloat(process.env.MASJID_LATITUDE ?? '49.2514'),
+  masjidLongitude: parseFloat(process.env.MASJID_LONGITUDE ?? '-122.7740'),
+  masjidTimezone: process.env.MASJID_TIMEZONE ?? 'America/Vancouver',
+  databaseUrl: process.env.DATABASE_URL,
 }));
 //# sourceMappingURL=app.config.js.map
